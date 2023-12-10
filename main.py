824c737605c60baa66309de9fec96f8099dbfa87
Anton <iasa.ka.92.marynych@gmail.com>
2023-12-11 01:00:13 +0200

    added base struct + imp of fetching and drowing data

---
+++ main.py
@@ -0,0 +1,89 @@
+
+import streamlit as st
+import pandas as pd
+from utils.data_processor import DataProcessor
+
+# Link to initial file with ds
+LINK = 'https://drive.google.com/uc?id=1tbgtf2us4bmZXWUDk9_EpgevwA1sI3Ri&export=download'
+DEM_COL = ['gender', 'SeniorCitizen', 'Partner', 'Dependents']
+ACC_COL = ['Contract', 'PaperlessBilling', 'PaymentMethod']
+SERV_COL = ['PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
+            'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+
+
+def main():
+	add_selectbox = st.sidebar.selectbox(
+	"How would you like to predict?",
+	("Online", "Batch"))
+	st.sidebar.info('This app is created to predict Customer Churn')
+	st.title("Predicting Customer Churn")
+	if add_selectbox == 'Online':
+		gender = st.selectbox('Gender:', ['male', 'female'])
+		seniorcitizen= st.selectbox(' Customer is a senior citizen:', [0, 1])
+		partner= st.selectbox(' Customer has a partner:', ['yes', 'no'])
+		dependents = st.selectbox(' Customer has  dependents:', ['yes', 'no'])
+		phoneservice = st.selectbox(' Customer has phoneservice:', ['yes', 'no'])
+		multiplelines = st.selectbox(' Customer has multiplelines:', ['yes', 'no', 'no_phone_service'])
+		internetservice= st.selectbox(' Customer has internetservice:', ['dsl', 'no', 'fiber_optic'])
+		onlinesecurity= st.selectbox(' Customer has onlinesecurity:', ['yes', 'no', 'no_internet_service'])
+		onlinebackup = st.selectbox(' Customer has onlinebackup:', ['yes', 'no', 'no_internet_service'])
+		deviceprotection = st.selectbox(' Customer has deviceprotection:', ['yes', 'no', 'no_internet_service'])
+		techsupport = st.selectbox(' Customer has techsupport:', ['yes', 'no', 'no_internet_service'])
+		streamingtv = st.selectbox(' Customer has streamingtv:', ['yes', 'no', 'no_internet_service'])
+		streamingmovies = st.selectbox(' Customer has streamingmovies:', ['yes', 'no', 'no_internet_service'])
+		contract= st.selectbox(' Customer has a contract:', ['month-to-month', 'one_year', 'two_year'])
+		paperlessbilling = st.selectbox(' Customer has a paperlessbilling:', ['yes', 'no'])
+		paymentmethod= st.selectbox('Payment Option:', ['bank_transfer_(automatic)', 'credit_card_(automatic)', 'electronic_check' ,'mailed_check'])
+		tenure = st.number_input('Number of months the customer has been with the current telco provider :', min_value=0, max_value=240, value=0)
+		monthlycharges= st.number_input('Monthly charges :', min_value=0, max_value=240, value=0)
+		totalcharges = tenure*monthlycharges
+		output= ""
+		output_prob = ""
+		input_dict={
+				"gender":gender ,
+				"seniorcitizen": seniorcitizen,
+				"partner": partner,
+				"dependents": dependents,
+				"phoneservice": phoneservice,
+				"multiplelines": multiplelines,
+				"internetservice": internetservice,
+				"onlinesecurity": onlinesecurity,
+				"onlinebackup": onlinebackup,
+				"deviceprotection": deviceprotection,
+				"techsupport": techsupport,
+				"streamingtv": streamingtv,
+				"streamingmovies": streamingmovies,
+				"contract": contract,
+				"paperlessbilling": paperlessbilling,
+				"paymentmethod": paymentmethod,
+				"tenure": tenure,
+				"monthlycharges": monthlycharges,
+				"totalcharges": totalcharges
+			}
+
+		if st.button("Predict"):
+			y_pred = model.predict_proba(X)[0, 1]
+			churn = y_pred >= 0.5
+			output_prob = float(y_pred)
+			output = bool(churn)
+		st.success('Churn: {0}, Risk Score: {1}'.format(output, output_prob))
+	if add_selectbox == 'Batch':
+		file_upload = st.file_uploader("Upload csv file for predictions", type=["csv"])
+		if file_upload is not None:
+			data = pd.read_csv(file_upload)
+			churn = y_pred >= 0.5
+			churn = bool(churn)
+			st.write(churn)
+
+
+def test():
+	dp = DataProcessor()
+	print(
+		dp.fetch_file(link=LINK)
+    )
+	dp.plot_correlation()
+
+	
+
+if __name__ == '__main__':
+	test()
